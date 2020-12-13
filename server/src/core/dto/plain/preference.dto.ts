a28a9cfa9c41b5c592ff402423f150ceaa9c4a8d
plsve <pulsivehm@seznam.cz>
2020-12-13 22:30:24 +0100

    Move DTO

--- server/src/core/dto/plain/preference.dto.ts
+++ server/src/core/dto/plain/preference.dto.ts
@@ -1,8 +1,12 @@
+import { IsString } from "class-validator";
+
 export class PreferenceDTO {
     id: number;
 
+    @IsString()
     theme: string;
 
+    @IsString()
     languageId: number;
 
     personId: number;
