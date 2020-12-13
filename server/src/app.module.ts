19f2214ef53521173e154784951e1ec647e2ff26
plsve <pulsivehm@seznam.cz>
2020-12-13 01:03:31 +0100

    Add env variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,23 +2,55 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import "reflect-metadata";
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { PersonController } from './core/controller/person.controller';
+import { PersonService } from './core/service/person.service';
+import { Person } from './core/model/person.entity';
+import { PermissionService } from './core/service/permission.service';
+import { Permission } from './core/model/permission.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Preference } from './core/model/preference.entity';
+import { Language } from './core/model/language.entity';
+import { Project } from './core/model/project.entity';
+import { Task } from './core/model/task.entity';
+import { DocFolder } from './core/model/doc-folder.entity';
+import { DocPage } from './core/model/doc-page.entity';
+import { DocFolderService } from './core/service/doc-folder.service';
+import { DocPageService } from './core/service/doc-page.service';
+import { LanguageService } from './core/service/language.service';
+import { PreferenceService } from './core/service/preference.service';
+import { ProjectService } from './core/service/project.service';
+import { TaskService } from './core/service/task.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: 'admin',
-      database: 'postgre_db',
-      entities: ["dist/**/*.entity{.ts,.js}"],
-      synchronize: true,
-      // keepConnectionAlive: true,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('TYPEORM_HOST'),
+        port: configService.get('TYPEORM_PORT'),
+        username: configService.get('TYPEORM_USERNAME'),
+        password: configService.get('TYPEORM_PASSWORD'),
+        database: configService.get('TYPEORM_DATABASE'),
+        entities: ["dist/**/*.entity{.ts,.js}"],
+        synchronize: configService.get('TYPEORM_SYNCHRONIZE'),
+        migrationsRun: false,
+        migrations: ["dist/core/migration/*.js"]
+      }),
+    }),
+    TypeOrmModule.forFeature([Project, DocFolder, DocPage, Task, Person, Permission, Preference, Language]),
+
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  exports: [
+
+  ],
+  controllers: [AppController, PersonController],
+  providers: [AppService, ConfigService,
+    ProjectService, DocFolderService, DocPageService, TaskService, PersonService, PermissionService, PreferenceService, LanguageService],
 })
-export class AppModule {}
+export class AppModule { }
