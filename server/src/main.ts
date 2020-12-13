35a2e91bf2ab46d82452a7d84c9658c36baf2827
plsve <pulsivehm@seznam.cz>
2020-12-13 22:31:53 +0100

    Add validation pipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true, // transforms incoming Javascript objects to their DTO classes
+    // disableErrorMessages: true,
+    // whitelist: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
