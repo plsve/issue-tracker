91a371e3f2e24e8972764f6969a533055e97c363
plsve <pulsivehm@seznam.cz>
2021-01-02 21:27:37 +0100

    Add side menu anchor

--- client/src/app/dash/dash.component.ts
+++ client/src/app/dash/dash.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
@@ -8,6 +8,10 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./dash.component.scss']
 })
 export class DashComponent implements OnInit {
+
+  @ViewChild('topMenu') elementView: ElementRef;
+
+  sideMenuClass = 'side-menu-wrap';
 
   project = {
     name: 'Starship-SN8'
@@ -48,6 +52,37 @@ export class DashComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    
+  }
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event) {
+    console.log(window.pageYOffset);
+    if(window.pageYOffset > this.elementView.nativeElement.offsetHeight){
+      this.sideMenuClass = 'side-menu-fixed';
+    } else {
+      this.sideMenuClass = 'side-menu-wrap';
+    }
+    // console.log(event);
+    // console.log(event.target.scrollingElement.clientTop);
+  }
+
+  getSideMenuStyle(){
+    if(window.pageYOffset > this.elementView.nativeElement.offsetHeight){
+      return 'side-menu-fixed';
+    } else {
+      return '';
+    }
+  }
+
+  getPositionStyle(){
+    // let pos = document.documentElement.scrollTop;
+    // console.log(pos);
+    // console.log(document.documentElement.scrollHeight);
+    // if(pos > 0){
+    //   return 'side-menu-fixed';
+    // }
+    
   }
 
   sideBtnClicked(event) {
